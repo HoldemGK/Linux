1b79bbb444d0ef4df0e8345af1c92d7dbd360249
Sanchez <skrolsky@gmail.com>
2019-12-15 21:22:39 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,3 +6,23 @@ export JENKINS_API_TOKEN=114288a57fd10bb8dd70edf6cbc9fd898c
 java -jar jenkins-cli.jar -s http://35.228.195.152:8080 get-job job-1 > job_1_back.xml
 #create new job from xml
 java -jar jenkins-cli.jar -s http://35.228.195.152:8080 create-job jobfromcli < job_1_back.xml
+
+# groovy scripts for jenkins
+//new File("${Jenkins.instance.root}/credentials.xml").text
+//Jenkins.instance.getNumExecutors()
+//Jenkins.instance.setNumExecutors(2)
+//job = Jenkins.instance.getItemByFullName("param-job")
+//job.getBuilds().each {
+//  if(it.result == Result.FAILURE) {
+//    it.delete()
+//  }
+//  println("Build " + it + " Result " + it.result)
+//}
+
+job = Jenkins.instance.getItemByFullName("build-autotriger-5-fromgit")
+job.builds.each() { build ->
+  build.delete()
+}
+
+job.update.NextBuildNumber(1)
+
