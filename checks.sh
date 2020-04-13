5c8dd3251fa145900cca8f2cd6670a24ddf829bb
Sanchez <skrolsky@gmail.com>
2020-04-13 17:26:57 +0300

    Update checks.sh

--- checks.sh
+++ checks.sh
@@ -1,3 +1,16 @@
+#Kernell version is currently running
+uname -a
+#Check size of the directory
+du -sh /home
+#Check open ports
+netstat -tulpn
+#Which process have /some/file open
+lsof /var/log/nginx/access.log
+#Where is the binary for process
+lsof -p 111 | grep bin # .so library, log logging
+#Which process is listening on port X
+lsof -i :80
+
 grep -F PRETTY_NAME /etc/*releas*
 `echo $SHELL` --version
 nginx -v
